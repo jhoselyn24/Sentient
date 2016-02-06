54a41883a095c5a79851e472ae9d5985afb6df9e
jhoselyn Inga <inga.jhoselyn@gmail.com>
2016-02-06 16:58:23 -0500

    cambios responsive

--- js/main.js
+++ js/main.js
@@ -12,3 +12,17 @@ $(function() {
     }
   });
 });
+
+$(document).ready(function(){
+
+  $(window).scroll(function(){
+    var barra = $(window).scrollTop();
+    var posicion =  (barra * 0.10);
+    
+    $('body').css({
+      'background-position': '0 -' + posicion + 'px'
+    });
+
+  });
+
+});
